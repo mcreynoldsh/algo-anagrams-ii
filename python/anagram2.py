a943e61b9e24a42d86aed48b772a17b064a776f5
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-25 18:16:07 -0500

    initial py solution

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,14 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
-		# your code here
+	word_chars = ''.join(sorted(word))
+	match_list = []
+	for i, x in enumerate(list_of_words):
+		test_chars = ''.join(sorted(x))
+		if(test_chars==word_chars):
+			match_list.append(x)
+
+	return match_list		
+		
+
+
